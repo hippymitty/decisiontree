94475749c5e4d96701be5bec40a6b6326dff5628
Sami <sami@Samis-MacBook-Pro-2.local>
2021-03-11 12:20:48 +1000

    updating readme and script

--- README.md
+++ README.md
@@ -18,3 +18,13 @@ pydotplus
 
 ### How to use
 Run the python files, and install any packages that show as 'module not found'. Once completed the print lines in the files will indicate if there's any errors, and at which point, otherwise it will produce the dot & PNG file
+
+
+
+### Errors/installs
+If youre running on jupyter notebooks you may encounter a graphviz error. To resolve, run:
+`! conda install python-graphviz`
+
+For Mac with the same or similar error, please run:
+`brew install graphviz`
+`pip install -U pydotplus`
--- tree.py
+++ tree.py
@@ -1,31 +1,26 @@
-# Load libraries
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import datasets
 from IPython.display import Image  
 from sklearn import tree
 import pydotplus
-# Load data
+
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 # Create decision tree classifer object
 clf = DecisionTreeClassifier(random_state=0)
-print("loaded, starting training")
-# Train model
+print("data loaded, training started")
+
 model = clf.fit(X, y)
-# Create DOT data
 dot_data = tree.export_graphviz(clf, out_file=None, 
                                 feature_names=iris.feature_names,  
                                 class_names=iris.target_names)
-print("trainging done")
-# Draw graph
-graph = pydotplus.graph_from_dot_data(dot_data)  
+print("trainging complete")
 
+
+graph = pydotplus.graph_from_dot_data(dot_data)  
 # Show graph
 Image(graph.create_png())
-
 # # Create PDF
 # graph.write_pdf("tree.pdf")
-
-# Create PNG
 graph.write_png("tree.png")
